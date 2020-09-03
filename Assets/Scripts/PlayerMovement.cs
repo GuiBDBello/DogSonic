9834c28fc5effff5dbe1f4cf11e1c6ce9f60d242
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-09-03 20:19:23 -0300

    Fix FPS Controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,29 +6,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public GameObject graphics;
-    public float speed = 12f;
-    public float gravity = -9.81f * 2.5f;
+    public float initialSpeed = 12f;
     public float jumpHeight = 5f;
-    public float playerHeight = 1.8f;
-
+    public float groundDistance = 1f;
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    Vector3 velocity;
-    bool isGrounded;
-    Camera mainCamera;
+    private Vector3 velocity;
+    private Camera mainCamera;
+
+    private float gravity;
+    private float speed;
+    private float playerHeight;
+    private bool isGrounded;
+    private bool isCrouched;
 
     private void Start()
     {
         mainCamera = Camera.main;
+        gravity = -9.81f * 2.5f;
+        speed = initialSpeed;
+        playerHeight = graphics.transform.localScale.y;
     }
 
     private void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0) Fall();
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -44,14 +49,10 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.LeftControl)) Crouch();
-        if (Input.GetKeyUp(KeyCode.LeftControl)) Stand();
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl)) Sprint();
-        if (Input.GetKeyUp(KeyCode.LeftShift)) Stand();
-    }
+        if (isCrouched && Input.GetKeyUp(KeyCode.LeftControl)) Stand();
 
-    private void Fall()
-    {
-        velocity.y = -2f;
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isCrouched) Sprint();
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !isCrouched) Walk();
     }
 
     private void Jump()
@@ -61,44 +62,27 @@ public class PlayerMovement : MonoBehaviour
 
     private void Crouch()
     {
-        Debug.Log(playerHeight);
+        isCrouched = true;
         controller.height = playerHeight;
-        //graphics.transform.localScale = new Vector3(graphics.transform.localScale.x, playerHeight / 2f, graphics.transform.localScale.z);
-        //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, (playerHeight / 2) - 0.1f, mainCamera.transform.position.z);
-        graphics.transform.localScale = new Vector3(
-            graphics.transform.localScale.x,
-            playerHeight / 2f,
-            graphics.transform.localScale.z
-        );
-        mainCamera.transform.position = new Vector3(
-            mainCamera.transform.position.x,
-            playerHeight * (playerHeight - 0.2f),
-            mainCamera.transform.position.z
-        );
-        speed = 12f / 2f;
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, 0.5f, gameObject.transform.localScale.z);
+        Walk();
     }
 
     private void Stand()
     {
-        Debug.Log(playerHeight);
+        isCrouched = false;
         controller.height = playerHeight * 2f;
-        //graphics.transform.localScale = new Vector3(graphics.transform.localScale.x, playerHeight - 0.1f, graphics.transform.localScale.z);
-        //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, playerHeight - 0.1f, mainCamera.transform.position.z);
-        graphics.transform.localScale = new Vector3(
-            graphics.transform.localScale.x,
-            playerHeight - 0.1f,
-            graphics.transform.localScale.z
-        );
-        mainCamera.transform.position = new Vector3(
-            mainCamera.transform.position.x,
-            playerHeight * (playerHeight - 0.1f),
-            mainCamera.transform.position.z
-        );
-        speed = 12f;
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, 1f, gameObject.transform.localScale.z);
+        Walk();
     }
 
     private void Sprint()
     {
-        speed = 12f * 2;
+        speed = initialSpeed * 2;
+    }
+
+    private void Walk()
+    {
+        speed = initialSpeed;
     }
 }
