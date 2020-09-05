79e973159c24876563632b775d7ea69fbb280cd2
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-09-04 21:48:33 -0300

    Create GameController, LevelController and SceneController scripts

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject Player;
+    public SceneController sceneController;
+
+    private Vector3 checkpoint;
+
+    void Start()
+    {
+        checkpoint = new Vector3(0f, 2f, 0f);
+    }
+
+    public void SetCheckpoint(Vector3 position)
+    {
+        this.checkpoint = position;
+    }
+
+    public void Respawn(GameObject player)
+    {
+        player.transform.position = checkpoint;
+    }
+
+    public void Finish()
+    {
+        sceneController.LoadMainMenu();
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
