5d54df28ab36017d0e000996de3200626f47414e
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-09-13 16:01:36 -0300

    Create Enemy and EnemyGenerator

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float timeToAutoDestroy = 5f;
+
+    private GameObject player;
+    private float timeRoaming = 0f;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.Player);
+    }
+    
+    private void Update()
+    {
+        float playerDistance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+        if (playerDistance < 20f)
+        {
+            timeRoaming = 0f;
+            gameObject.transform.LookAt(Vector3.Lerp(gameObject.transform.position, player.transform.position, 0.0005f));
+        }
+        else timeRoaming += Time.deltaTime;
+
+        if (timeRoaming >= timeToAutoDestroy) Destroy(gameObject);
+
+        gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    private void Start()
+    {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        for (;;)
+        {
+            Instantiate(Enemy, transform.position, transform.parent.rotation);
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
