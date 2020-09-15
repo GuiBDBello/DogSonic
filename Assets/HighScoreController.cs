309575a152449bde39cea69b3c1decf7226ad32f
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-09-14 22:40:21 -0300

    Update some prefabs; Create HighScoreController script

---
+++ Assets/HighScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreController : MonoBehaviour
+{
+    public Text HighScoreLevel01;
+    public Text HighScoreLevel02;
+    public Text HighScoreLevel03;
+    public Text HighScoreLevel04;
+
+    private void Start()
+    {
+        HighScoreLevel01.text = "High Score: " + PlayerPrefs.GetFloat(Scenes.Level01);
+        HighScoreLevel02.text = "High Score: " + PlayerPrefs.GetFloat(Scenes.Level02);
+        HighScoreLevel03.text = "High Score: " + PlayerPrefs.GetFloat(Scenes.Level03);
+        HighScoreLevel04.text = "High Score: " + PlayerPrefs.GetFloat(Scenes.Level04);
+    }
+}
